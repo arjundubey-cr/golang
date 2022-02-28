885c4274a6febe8b78bde1384878b72890746790
Arjun Dubey <arjundubeycr@gmail.com>
2022-02-28 11:10:53 +0530

    array and slices

--- 08.Array/main.go
+++ 08.Array/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := [...]int{1, 2, 3}
+	fmt.Println(arr)
+
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	b := a[:]  //slice all of the elements
+	c := a[3:] //slice from 4th to the end
+	d := a[:6] //slice first 6
+	e := a[3:6]
+	f := append(a[:2], a[3:]...)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+
+	fmt.Println(f)
+	fmt.Println(a) //unexpected behaviour here
 }
